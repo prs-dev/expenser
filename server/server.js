3c77da1e5869ac70b3e152e64dbb8603b02fe4b9
prs-dev <pratyushsri.25@gmail.com>
2026-02-13 17:07:39 +0530

    all expenses endpoint created

--- server/server.js
+++ server/server.js
@@ -1,8 +1,38 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Expense = require('./expense.model')
 require('dotenv').config()
 
 const app = express()
+
+app.post("/create", (req, res) => {
+    try {
+        const {amt, category, date, note} = req.body
+        if(!amt || !date) {
+            return res.status(400).json({
+                msg: "please provide all the fields!"
+            })
+        }
+        return res.status(200).json({
+            msg: "success",
+            expenses: allExpenses
+        })
+    } catch (error) {
+        console.log("error in creating expense", error)
+    }
+})
+
+app.get('/all', async(req, res) => {
+    try {
+        const allExpenses = await Expense.find({})
+        return res.status(200).json({
+            msg: "success",
+            expenses: allExpenses
+        })
+    } catch (error) {
+        console.log("error in getting all expenses", error)
+    }
+})
 
 app.get("/", (req, res) => {
     res.send('hello from server')
